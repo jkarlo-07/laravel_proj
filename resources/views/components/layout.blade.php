b8a39c3de4eed5d84d9e88aee307b0713d875147
jkarlo07 <karlojohn721@gmail.com>
2025-04-30 12:50:37 +0800

    create a layout using laravel components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>About Page</h1>
+    <nav>
+        <x-nav-link>Home</x-nav-link>
+        <x-nav-link>About</x-nav-link>
+        <x-nav-link>Contact</x-nav-link>
+    </nav>
+
+    {{ $slot }}
 </body>
+
 </html>
