e8e7785ce6d777b72861db438dd31120045a43b4
jkarlo07 <karlojohn721@gmail.com>
2025-05-01 16:18:35 +0800

    autoloading, namespace and models

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+
 
 Route::get('/', function () {
     return view('home', [
@@ -12,48 +15,12 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$500,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ],
-
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$500,000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000',
-        ],
-
-    ];
-
-    $job= Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id){
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
