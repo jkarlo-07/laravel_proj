aff9f1d74c54a664045e3dc83ffaf5a49e5a2bbe
jkarlo07 <karlojohn721@gmail.com>
2025-05-02 16:46:43 +0800

    pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         Model::preventLazyLoading();
+
+        // Paginator::useBootstrapFive();
     }
 
     /**
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -11,6 +11,10 @@
                 </div>
             </a>
         @endforeach
+
+				<div>
+					{{$jobs->links()}}
+				</div>
     </div>
 
 </x-layout>
