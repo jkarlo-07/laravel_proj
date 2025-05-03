aa6a027b2c6fdce2865989f2cb1675ddec0fbf88
jkarlo07 <karlojohn721@gmail.com>
2025-05-03 17:14:33 +0800

    edit, update and delete

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 
-
+// Index
 Route::get('/', function () {
     return view('home');
 });
@@ -19,16 +19,20 @@ Route::get('/jobs', function () {
     ]);
 });
 
+
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
-
+// Show
 Route::get('/jobs/{id}', function ($id){
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+
+// Store
 Route::post('/jobs',function () {
 
     request()->validate([
@@ -42,6 +46,43 @@ Route::post('/jobs',function () {
         'employer_id' => 2,
     ]);
 
+    return redirect('/jobs');
+});
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id){
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update
+Route::patch('/jobs/{id}', function ($id){
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    //authorization (on hold)
+
+    $job = Job::findOrFail($id);
+
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id){
+    // authorize (on hold...)
+    // delete the job
+    Job::findOrFail($id)->delete();
     return redirect('/jobs');
 });
 
